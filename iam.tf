63b5eac818aa593e3fbbd2dea888314a9e5e0c2d
Arthur <artcosta1@gmail.com>
2024-11-15 15:49:17 -0300

    new: configurations

--- iam.tf
+++ iam.tf
@@ -50,6 +50,12 @@ resource "aws_iam_role_policy" "tf_permissions" {
         Action   = "iam:*",
         Effect   = "Allow",
         Resource = "*"
+      },
+      {
+        Sid      = "Statement3"
+        Action   = "s3:*"
+        Effect   = "Allow"
+        Resource = "*"
       }
     ]
   })
--- main.tf
+++ main.tf
@@ -5,8 +5,38 @@ terraform {
       version = "5.76.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "rocketseat-iac-cursodocker"
+    key     = "state/terraform.tfstate"
+    region  = "us-east-2"
+    # profile = "AdministratorAccess-651706743475"
+
+  }
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region  = "us-east-2"
+  # profile = "AdministratorAccess-651706743475"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "rocketseat-iac-cursodocker"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "rocketseat-iac-cursodocker"
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
