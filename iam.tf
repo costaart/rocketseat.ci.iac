bb2159c275ebfbccd76f0f2998e8ca2b3b584363
Arthur <artcosta1@gmail.com>
2024-11-15 15:16:56 -0300

    fix: fmt 2

--- iam.tf
+++ iam.tf
@@ -1,12 +1,7 @@
 resource "aws_iam_openid_connect_provider" "oidc-git" {
-  url = "https://token.actions.githubusercontent.com"
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "1b511abead59c6ce207077c0bf0e0043b1382612"
-  ]
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
 
   tags = {
     IAC = "True"
@@ -15,7 +10,6 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
 
 resource "aws_iam_role" "tf-role" {
   name = "tf-role"
-
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
@@ -27,132 +21,36 @@ resource "aws_iam_role" "tf-role" {
         },
         "Condition" : {
           "StringEquals" : {
-            "token.actions.githubusercontent.com:aud" : [
-              "sts.amazonaws.com"
-            ],
-            "token.actions.githubusercontent.com:sub" : [
-              "repo:costaart/rocketseat.ci.iac:ref:refs/heads/main"
-            ]
+            "token.actions.githubusercontent.com:aud" : ["sts.amazonaws.com"],
+            "token.actions.githubusercontent.com:sub" : ["repo:costaart/rocketseat.ci.iac:ref:refs/heads/main"]
           }
         }
       }
     ]
   })
 
-  inline_policy {
-    name = "tf-permissions"
-    policy = jsonencode({
-      Statement = [{
-        Sid      = "Statement1"
-        Action   = "ecr:*"
-        Effect   = "Allow"
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_iam_role_policy" "tf_permissions" {
+  name = "tf-permissions"
+  role = aws_iam_role.tf-role.name
+  policy = jsonencode({
+    Statement = [
+      {
+        Sid      = "Statement1",
+        Action   = "ecr:*",
+        Effect   = "Allow",
         Resource = "*"
-        },
-        {
-          Sid      = "Statement2"
-          Action   = "iam:*"
-          Effect   = "Allow"
-          Resource = "*"
-        }
-      ]
-    })
-  }
-
-  tags = {
-    IAC = "True"
-  }
-}
-
-resource "aws_iam_role" "app-runner-role" {
-  name = "app-runner-rule"
-
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+      },
       {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "build.apprunner.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
+        Sid      = "Statement2",
+        Action   = "iam:*",
+        Effect   = "Allow",
+        Resource = "*"
       }
     ]
   })
-
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-  ]
-
-  tags = {
-    IAC = "True"
-  }
-
-}
-
-resource "aws_iam_role" "ecr-role" {
-  name = "ecr-role"
-
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : "sts:AssumeRoleWithWebIdentity",
-        "Principal" : {
-          "Federated" : "arn:aws:iam::651706743475:oidc-provider/token.actions.githubusercontent.com"
-        },
-        "Condition" : {
-          "StringEquals" : {
-            "token.actions.githubusercontent.com:aud" : [
-              "sts.amazonaws.com"
-            ],
-            "token.actions.githubusercontent.com:sub" : [
-              "repo:costaart/rocketseat.ci.cursodocker:ref:refs/heads/main"
-            ]
-          }
-        }
-      }
-    ]
-  })
-
-  inline_policy {
-    name = "ecr-app-permissions"
-    policy = jsonencode({
-      Statement = [
-        {
-          Sid = "Statement1",
-          Action = [
-            "iam:PassRole",
-            "iam:CreateServiceLinkedRole"
-          ],
-          Effect   = "Allow",
-          Resource = "*"
-        },
-        {
-          Sid      = "Statement2",
-          Action   = "apprunner:*",
-          Effect   = "Allow",
-          Resource = "*"
-        },
-        {
-          Sid = "Statement3"
-          Action = [
-            "ecr:GetDownloadUrlForLayer",
-            "ecr:BatchGetImage",
-            "ecr:BatchCheckLayerAvailability",
-            "ecr:PutImage",
-            "ecr:InitiateLayerUpload",
-            "ecr:UploadLayerPart",
-            "ecr:CompleteLayerUpload",
-            "ecr:GetAuthorizationToken",
-          ]
-          Effect   = "Allow"
-          Resource = "*"
-        }
-      ]
-    })
-  }
-  tags = {
-    IAC = "True"
-  }
 }
