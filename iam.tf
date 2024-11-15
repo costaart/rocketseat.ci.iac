302e123be7592b71d119eec0f75dadffa3cdac5c
Arthur <artcosta1@gmail.com>
2024-11-15 15:08:01 -0300

    new: role config

--- iam.tf
+++ iam.tf
@@ -7,7 +7,37 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   thumbprint_list = [
     "1b511abead59c6ce207077c0bf0e0043b1382612"
   ]
-  
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::651706743475:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ],
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:costaart/rocketseat.ci.iac:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+  })
   tags = {
     IAC = "True"
   }
@@ -17,14 +47,14 @@ resource "aws_iam_role" "app-runner-role" {
   name = "app-runner-rule"
 
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "build.apprunner.amazonaws.com"
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "build.apprunner.amazonaws.com"
         },
-        "Action": "sts:AssumeRole"
+        "Action" : "sts:AssumeRole"
       }
     ]
   })
@@ -43,27 +73,27 @@ resource "aws_iam_role" "ecr-role" {
   name = "ecr-role"
 
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Principal": {
-                "Federated": "arn:aws:iam::651706743475:oidc-provider/token.actions.githubusercontent.com"
-            },
-            "Condition": {
-                "StringEquals": {
-                    "token.actions.githubusercontent.com:aud": [
-                        "sts.amazonaws.com"
-                    ],
-                    "token.actions.githubusercontent.com:sub": [
-                        "repo:costaart/rocketseat.ci.cursodocker:ref:refs/heads/main"
-                    ]
-                }
-            }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::651706743475:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ],
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:costaart/rocketseat.ci.cursodocker:ref:refs/heads/main"
+            ]
+          }
         }
+      }
     ]
-})
+  })
 
   inline_policy {
     name = "ecr-app-permissions"
@@ -75,14 +105,14 @@ resource "aws_iam_role" "ecr-role" {
             "iam:PassRole",
             "iam:CreateServiceLinkedRole"
           ],
-          Effect = "Allow",
-          Resource = "*" 
+          Effect   = "Allow",
+          Resource = "*"
         },
         {
-          Sid = "Statement2",
-          Action = "apprunner:*",
-          Effect = "Allow",
-          Resource = "*" 
+          Sid      = "Statement2",
+          Action   = "apprunner:*",
+          Effect   = "Allow",
+          Resource = "*"
         },
         {
           Sid = "Statement3"
